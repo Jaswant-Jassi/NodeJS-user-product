a903c2079aa2b1aaee04e00f720d5202581ec34f
Jaswant <jaswantkhemani@gmail.com>
2023-12-14 16:31:00 +0500

    role added

--- api/schemas/userschema.js
+++ api/schemas/userschema.js
@@ -1,26 +1,15 @@
 const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema({
-    username: { 
-        type: String, required: true 
-    },
-    email: { 
-        type: String, required: true, unique: true 
-    },
-    password: { 
-        type: String, required: true 
-    },
-    gender: { 
-        type: String, required: true 
-    },
-    profile_pic: { 
-        type: String, default: "https://cdn-icons-png.flaticon.com/512/1144/1144760.png" 
-    },
-    joining: { 
-        type: Date, default: Date.now 
-    }
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    gender: { type: String, required: true },
+    profile_pic: { type: String, default: "https://cdn-icons-png.flaticon.com/512/1144/1144760.png" },
+    joining: { type: Date, default: Date.now },
+    role: {type: String, default:"user"}
 
 })
 
 const user = model('user', userSchema)
-module.exports = user 
+module.exports = user  
