11bec1f2a052dbd040be837efe81477df2ebb41e
Jaswant <jaswantkhemani@gmail.com>
2023-12-20 13:29:02 +0500

    Update productschema.js

--- api/schemas/productschema.js
+++ api/schemas/productschema.js
@@ -1,22 +1,24 @@
 const { Schema, model } = require('mongoose')
 
 const productSchema = new Schema({
+    images: {
+        type: String, required: true, default: "data:https://icons8.com/icon/115643/unavailable"
+    },
     productname: {
         type: String, required: true, unique: true
+    },
+    author: {
+        type: String, required: true 
     },
     price: {
         type: Number, required: true
     },
-    images: {
-        type: String, required: true, default: "data:https://icons8.com/icon/115643/unavailable"
-    },
-    brand: {
-        type: String, required: true 
-    },
     category: {
         type: String, required: true
     },
-
+    publisher: {
+        type: String, required: true
+    },
 
 })
 
