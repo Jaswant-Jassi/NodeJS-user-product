ef0ced67e6eabe76e6ce7e9db64818d636a65f22
Jaswant <jaswantkhemani@gmail.com>
2023-12-23 18:44:53 +0500

    updated p.schema

---
+++ productschema.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose')
+
+const productSchema = new Schema({
+    images: {
+        type: String, required: true, default: "data:https://icons8.com/icon/115643/unavailable"
+    },
+    price: {
+        type: Number, required: true
+    },
+    productname: {
+        type: String, required: true, unique: true
+    },
+    author: {
+        type: String, required: true 
+    },
+    category: {
+        type: String, required: true
+    },
+    publisher: {
+        type: String, required: true 
+    },
+    publicationyear: {
+        type: String, required: true 
+    },
+
+
+})
+
+const product = model('product', productSchema)
+module.exports = product
+
+
+
+
+
+
+
+ 
+
+
+
+
